957257668fcfefb105179eb4144de346e956e666
Divyansh Sharma <85435719+Dsharma2002@users.noreply.github.com>
2022-04-12 19:31:48 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const Recipe = require("./models/recipe")
+const methodOverride = require("method-override")
 
 // set view engine
 app.set("view engine", "ejs")
 
 // middleware
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
 
 const URL = `mongodb+srv://divi2002:divyansh@cluster0.wwyhn.mongodb.net/Cluster0?retryWrites=true&w=majority`
 mongoose.connect(URL)
@@ -30,20 +32,20 @@ app.get("/recipes/create", (req, res) => {
     res.render("create", { title: "CREATE" })
 })
 
-app.post("/recipes", (req, res) => {
-    const recipe = new Recipe(req.body)
-    recipe.save()
-        .then(result => res.redirect("/recipes"))
-        .catch(err => console.log(err))
+app.get("/recipes/:id", async (req, res) => {
+    const id = req.params.id
+    const recipe = await Recipe.findById(id)
+    if (recipe == null) {
+        res.redirect("/recipes")
+    } else {
+        res.render("details", { title: "Recipe Details", recipe: recipe })
+    }
 })
 
-app.get("/recipes/:id", (req, res) => {
+app.get("/recipes/edit/:id", async (req, res) => {
     const id = req.params.id
-    Recipe.findById(id)
-        .then(result => {
-            res.render("details", { title: "Recipe Details", recipe: result })
-        })
-        .catch(err => console.log(err))
+    const recipe = await Recipe.findById(id)
+    res.render("edit", { recipe: recipe, title: "EDIT" })
 })
 
 app.delete("/recipes/:id", (req, res) => {
@@ -52,6 +54,35 @@ app.delete("/recipes/:id", (req, res) => {
         .then(result => res.json({ redirect: "/recipes" }))
         .catch(err => console.log(err))
 })
+
+app.post("/recipes", (req, res) => {
+    const recipe = new Recipe(req.body)
+    recipe.save()
+        .then(result => res.redirect("/recipes"))
+        .catch(err => console.log(err))
+})
+
+app.put("/recipes/:id", async (req, res, next) => {
+    const id = req.params.id
+    req.recipe = await Recipe.findById(id)
+    next()
+}, saveRecipe('edit'))
+
+function saveRecipe(path) {
+    return async (req, res) => {
+        let recipe = req.recipe
+        recipe.name = req.body.name
+        recipe.cookTime = req.body.cookTime
+        recipe.ingredients = req.body.ingredients
+        recipe.procedure = req.body.procedure
+        try {
+            recipe = await recipe.save()
+            res.redirect(`/recipes/${recipe.id}`)
+        } catch (err) {
+            res.render(`/recipes/${path}`, { recipe: recipe })
+        }
+    }
+}
 
 // 404
 app.use((req, res) => {
